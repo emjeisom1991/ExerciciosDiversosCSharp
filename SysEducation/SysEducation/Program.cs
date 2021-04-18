fe7b91b20a14dfe9e083828c605c94e76aa68ea4
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-17 21:58:09 -0300

    SysEducation: Created of SysEducation

---
+++ SysEducation/SysEducation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysEducation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
