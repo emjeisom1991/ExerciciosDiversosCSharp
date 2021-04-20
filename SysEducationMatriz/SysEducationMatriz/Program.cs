23edfbb566f591b357a54d10ce6e5fb4cefada1b
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-19 21:49:37 -0300

    SysEducationMatriz: Finished

--- SysEducationMatriz/SysEducationMatriz/Program.cs
+++ SysEducationMatriz/SysEducationMatriz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace SysEducationMatriz
 {
@@ -6,7 +7,60 @@ namespace SysEducationMatriz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[,] StuInfo = new double[5, 4];
+            double GFGrade = 0, GRegistration = 0, AFGrade=0, contStu=0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    StuInfo[i, j] = 0;
+                }
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{i+1}º Aluno: ");
+                string[] StuData = Console.ReadLine().Split(' ');
+                for (int j = 0; j < 3; j++)
+                {
+                    StuInfo[i, j] = double.Parse(StuData[j]);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    StuInfo[i, 3] = (StuInfo[i, 1] + StuInfo[i, 2]) / 2;
+
+                    if (StuInfo[i, 3] >= GFGrade)
+                    {
+                        GFGrade = StuInfo[i, 3];
+                        GRegistration = StuInfo[i, 0];
+                    }
+                        
+
+                    
+                }
+                AFGrade += StuInfo[i, 3];
+                contStu++;
+            }
+
+            Console.WriteLine($"The greatest final grade belongs to registration: {GRegistration}");
+            Console.WriteLine($"Arithmetic average of final grades: {(AFGrade/contStu).ToString("F2", CultureInfo.InvariantCulture)}");
+
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        Console.Write($"{StuInfo[i, j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
         }
     }
 }
