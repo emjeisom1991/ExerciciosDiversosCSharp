294bd68e0c37f44d3d1effe67618a537f1bfece7
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-20 11:48:07 -0300

    StandardDeviationVet: Finished

--- StandardDeviationVet/StandardDeviationVet/Program.cs
+++ StandardDeviationVet/StandardDeviationVet/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Program that calculates a Standard Deviation of a vector with n numbers
+
+using System;
+using System.Globalization;
 
 namespace StandardDeviationVet
 {
@@ -6,7 +9,30 @@ namespace StandardDeviationVet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How many numbers is there in the vector ? ");
+            int qtd = int.Parse(Console.ReadLine());
+            double[] StaDev = new double[qtd];
+            double n = 0, m = 0, sum = 0, sd = 0;
+
+            Console.Clear();
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.Write($"Set number {i+1}: ");
+                StaDev[i] = double.Parse(Console.ReadLine());
+                sum += StaDev[i];
+            }
+
+            m = sum/qtd;          
+
+            for (int i = 0; i < qtd; i++)
+            {
+                sd += Math.Pow((StaDev[i] - m), 2);
+            }
+
+            Console.Clear();
+            Console.Write("Standard Deviation = ");
+            Console.WriteLine(Math.Sqrt( sd/(qtd-1)).ToString("F6", CultureInfo.InvariantCulture));
         }
     }
 }
