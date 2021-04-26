56c198a44bfb866447ad846945277295069ed548
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-26 14:40:26 -0300

    Mult07: Paused

--- Mult07/Mult07/Program.cs
+++ Mult07/Mult07/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Faça um programa que preencha um vetor de tamanho 100 com os 100
+// primeiros naturais que não são múltiplos de 7 ou que terminam com 7.
+
+using System;
 
 namespace Mult07
 {
@@ -6,7 +9,26 @@ namespace Mult07
     {
         static void Main(string[] args)
         {
-            
+            int n = 100, m = 0;
+            int[] vet = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 7 != 0)
+                {
+                    m++;
+                    vet[i] = m;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i+1}ª posicao: {vet[i]}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("REPETIÇÔES: " + m);
+
         }
     }
 }
