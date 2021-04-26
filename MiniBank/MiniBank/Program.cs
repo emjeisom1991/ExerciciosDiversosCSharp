2a2b84d0a4f065824ff65a90d77887d7c8a1db83
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-26 15:02:54 -0300

    MiniBank: Created

---
+++ MiniBank/MiniBank/Program.cs
@@ -0,0 +1,14 @@
+﻿// Escreva	um programa que apresente quatro opções: (a) consulta saldo, (b) saque e (c) depósito e (d) sair.
+// O saldo deve iniciar em R$ 0,00. A cada saque ou depósito o valor do saldo deve ser atualizado.
+using System;
+
+namespace MiniBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
