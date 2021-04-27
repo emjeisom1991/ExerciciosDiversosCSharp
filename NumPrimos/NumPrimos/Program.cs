09b22c04eead2dec3436a0bdbc134a239d09bb9d
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-27 12:31:11 -0300

    NumPrimos: Created

---
+++ NumPrimos/NumPrimos/Program.cs
@@ -0,0 +1,14 @@
+﻿// Leia 10 numeros inteiros e armazene em um vetor. Em seguida escreva os elementos que sao
+// primos e suas respectivas posições no vetor.
+using System;
+
+namespace NumPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
