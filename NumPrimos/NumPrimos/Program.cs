6728e0fa609d237861219b4434eea1fb86b3a27f
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-27 13:31:15 -0300

    NumPrimos: declaration of variables

--- NumPrimos/NumPrimos/Program.cs
+++ NumPrimos/NumPrimos/Program.cs
@@ -8,7 +8,24 @@ namespace NumPrimos
     {
         static void Main(string[] args)
         {
-            
+            int pos = 10;
+            int cont = 0, n = 0;
+            Boolean primo = false;
+            int[] vet = new int[pos];
+
+            Console.Write("Entre com os " + pos + " numeros: ");
+            string[] num = Console.ReadLine().Split(' ');
+            for (int i = 0; i < pos; i++)
+            {
+                vet[i] = int.Parse(num[i]);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < pos; i++)
+            {
+                
+            }
+
         }
     }
 }
