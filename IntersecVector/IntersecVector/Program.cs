f4c617f0989166a0efde39f88616efc2304534b4
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-28 13:49:05 -0300

    IntersecVector: Created

---
+++ IntersecVector/IntersecVector/Program.cs
@@ -0,0 +1,15 @@
+﻿// Faça um programa que leia dois vetores de 10 elementos. Crie um vetor que seja a intersecção entre
+// os 2 vetores anteriores, ou seja, que contém apenas os números que estão em ambos os vetores. Não
+// deve conter números repetidos.
+using System;
+
+namespace IntersecVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
