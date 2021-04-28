b148c26cf8943a6ac836cf483a94a18af2f80118
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-04-28 14:16:11 -0300

    IntersecVector: Finished

--- IntersecVector/IntersecVector/Program.cs
+++ IntersecVector/IntersecVector/Program.cs
@@ -9,7 +9,53 @@ namespace IntersecVector
     {
         static void Main(string[] args)
         {
-            
+            int len = 5, n = 0, cont = 0;
+            int[] vec01 = new int[len];
+            int[] vec02 = new int[len];
+            int[] intersect = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                vec01[i] = 0;
+                vec02[i] = 0;
+            }
+
+            Console.Write("Informe os dados do 1º vetor: ");
+            string[] numbers = Console.ReadLine().Split(' ');
+            for (int i = 0; i < len; i++)
+            {
+                vec01[i] = int.Parse(numbers[i]);
+            }
+
+            Console.Write("Informe os dados do 2º vetor: ");
+            numbers = Console.ReadLine().Split(' ');
+            for (int i = 0; i < len; i++)
+            {
+                vec02[i] = int.Parse(numbers[i]);
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    if (vec01[i] == vec02[j])
+                    {
+                        //n = vec01[i];
+                        intersect[cont] = vec01[i];
+                        cont++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write("Vetor intersecção: ");
+            for (int i = 0; i < cont; i++)
+            {
+                Console.Write($"{intersect[i]} ");
+            }
+
+            Console.WriteLine();
+
         }
     }
 }
