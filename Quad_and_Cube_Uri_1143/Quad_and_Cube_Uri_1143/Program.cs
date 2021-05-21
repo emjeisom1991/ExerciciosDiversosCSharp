a464746b67367ecab156d56e4a0f86b09c122aa2
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-21 10:45:14 -0300

    Quad_and_Cube_Uri_1143: Finished

--- Quad_and_Cube_Uri_1143/Quad_and_Cube_Uri_1143/Program.cs
+++ Quad_and_Cube_Uri_1143/Quad_and_Cube_Uri_1143/Program.cs
@@ -6,7 +6,12 @@ namespace Quad_and_Cube_Uri_1143
     {
         static void Main(string[] args)
         {
-            
+            int N = int.Parse(Console.ReadLine());
+
+            for(int i = 1;i <= N; i++)
+            {
+                Console.WriteLine(i + " " + Math.Pow(i, 2) + " " + Math.Pow(i, 3));
+            }
         }
     }
 }
