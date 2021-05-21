559a5a016a7c47deb0b77ba60e76a2b5e103174d
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-21 10:50:34 -0300

    Sequencia_IJ_1_Uri_1095: Created

---
+++ Sequencia_IJ_1_Uri_1095/Sequencia_IJ_1_Uri_1095/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sequencia_IJ_1_Uri_1095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
