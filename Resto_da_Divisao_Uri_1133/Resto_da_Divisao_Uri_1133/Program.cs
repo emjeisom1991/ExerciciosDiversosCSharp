5d6e62d69e02c185de0775da438ea7a1a302f423
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-23 17:11:59 -0300

    Resto_da_Divisao_Uri_1133: Finished

--- Resto_da_Divisao_Uri_1133/Resto_da_Divisao_Uri_1133/Program.cs
+++ Resto_da_Divisao_Uri_1133/Resto_da_Divisao_Uri_1133/Program.cs
@@ -6,7 +6,21 @@ namespace Resto_da_Divisao_Uri_1133
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Informe os valores de x e y: ");
+            string[] xy = Console.ReadLine().Split(" ");
+            int x = int.Parse(xy[0]);
+            int y = int.Parse(xy[1]);
+
+            for (int i = x; i < y; i++)
+            {
+                if(i % 5 == 2 || i % 5 == 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
         }
     }
 }
