f688d99474c273772980469e800498141b110779
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-23 17:06:04 -0300

    Resto_da_Divisao_Uri_1133: created

--- FibonacciFacil_Uri_1151/FibonacciFacil_Uri_1151/Program.cs
+++ FibonacciFacil_Uri_1151/FibonacciFacil_Uri_1151/Program.cs
@@ -6,7 +6,30 @@ namespace FibonacciFacil_Uri_1151
     {
         static void Main(string[] args)
         {
-            
+
+            double t1 = 2, t2 = 3, t3;
+
+            Console.Write("Informe quantos termos terá a sequencia: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 46)
+            {
+                Console.Write("0 1 1 2 3 ");
+                for (int i = 2; i <= (n - 4); i++)
+                {
+                    t3 = t1 + t2;
+                    Console.Write(t3 + " ");
+                    t1 = t2;
+                    t2 = t3;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Impossivel calcular.");
+            }
+
+            Console.WriteLine();
+
         }
     }
 }
---
+++ Resto_da_Divisao_Uri_1133/Resto_da_Divisao_Uri_1133/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Resto_da_Divisao_Uri_1133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
