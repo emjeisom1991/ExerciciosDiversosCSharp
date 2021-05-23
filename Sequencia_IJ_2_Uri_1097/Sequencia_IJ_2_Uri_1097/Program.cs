9973bef399ac377db5bd0e598b02b1e400834930
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-22 21:57:57 -0300

    Sequencia_IJ_2_Uri_1097: Finished

--- Sequencia_IJ_2_Uri_1097/Sequencia_IJ_2_Uri_1097/Program.cs
+++ Sequencia_IJ_2_Uri_1097/Sequencia_IJ_2_Uri_1097/Program.cs
@@ -6,7 +6,17 @@ namespace Sequencia_IJ_2_Uri_1097
     {
         static void Main(string[] args)
         {
-            
+            int sum = 0;
+
+            for (int i = 1; i <= 9; i += 2)
+            {
+                for (int j = 7; j >= 5; j--)
+                {
+                    Console.WriteLine("I=" + i + " J=" + (j + sum));
+                }
+                sum+=2;
+                Console.WriteLine();
+            }
         }
     }
 }
