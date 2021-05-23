fb5f2423f084e644876273027e17c2b22c00a260
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-22 21:47:43 -0300

    Sequencia_IJ_2_Uri_1096: Finished

--- Sequencia_IJ_2_Uri_1096/Sequencia_IJ_2_Uri_1096/Program.cs
+++ Sequencia_IJ_2_Uri_1096/Sequencia_IJ_2_Uri_1096/Program.cs
@@ -6,7 +6,13 @@ namespace Sequencia_IJ_2_Uri_1096
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 1; i <= 9; i+=2)
+            {
+                for (int j = 7; j >= 5; j--)
+                {
+                    Console.WriteLine("I=" + i + " J=" + j);
+                }
+            }
         }
     }
 }
