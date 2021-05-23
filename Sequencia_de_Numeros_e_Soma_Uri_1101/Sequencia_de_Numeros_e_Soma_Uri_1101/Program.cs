4bdd61a6e34f4ef577973e9bd9e63094f7c0e3a7
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-23 17:43:35 -0300

    Sequencia_de_Numeros_e_Soma_Uri_1101: Finished

--- Sequencia_de_Numeros_e_Soma_Uri_1101/Sequencia_de_Numeros_e_Soma_Uri_1101/Program.cs
+++ Sequencia_de_Numeros_e_Soma_Uri_1101/Sequencia_de_Numeros_e_Soma_Uri_1101/Program.cs
@@ -6,7 +6,42 @@ namespace Sequencia_de_Numeros_e_Soma_Uri_1101
     {
         static void Main(string[] args)
         {
-            
+            int M, N, sum = 0, maior = 0, menor;
+            string[] MN;
+
+            do
+            {
+
+                MN = Console.ReadLine().Split(" ");
+                M = int.Parse(MN[0]);
+                N = int.Parse(MN[1]);
+
+                if (M > N)
+                {
+                    maior = M;
+                    menor = N;
+                }
+                else
+                {
+                    maior = N;
+                    menor = M;
+                }
+
+                if (M > 0 && N > 0)
+                {
+
+                    for (int i = menor; i <= maior; i++)
+                    {
+                        Console.Write(i + " ");
+                        sum += i;
+                    }
+
+                    Console.WriteLine("Sum=" + sum);
+                }
+
+                sum = 0;
+
+            } while (M > 0 && N > 0);
         }
     }
 }
