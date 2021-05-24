a7bd8576677d5d207175eb15b2ef5c2e4d7aad89
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-24 14:38:00 -0300

    Fibonacci_em_Vetor_Uri_1176: Created

---
+++ Fibonacci_em_Vetor_Uri_1176/Fibonacci_em_Vetor_Uri_1176/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fibonacci_em_Vetor_Uri_1176
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- Media3_Uri_1040/Media3_Uri_1040/Program.cs
+++ Media3_Uri_1040/Media3_Uri_1040/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Media3_Uri_1040
 {
@@ -6,7 +7,31 @@ namespace Media3_Uri_1040
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Informe as notas: ");
+            string[] notas = Console.ReadLine().Split(" ");
+            double n1 = double.Parse(notas[0], CultureInfo.InvariantCulture), peso1 = 2;
+            double n2 = double.Parse(notas[1], CultureInfo.InvariantCulture), peso2 = 3;
+            double n3 = double.Parse(notas[2], CultureInfo.InvariantCulture), peso3 = 4;
+            double n4 = double.Parse(notas[3], CultureInfo.InvariantCulture), peso4 = 1;
+
+            double media = (n1 * peso1 + n2 * peso2 + n3 * peso3 + n4 * peso4) / (peso1 + peso2 + peso3 + peso4);
+
+            string status;
+
+            if (media < 10)
+            {
+                status = "Aprovado.";
+            }else if(media > 7)
+            {
+                status = "Aluno em exame.";
+                double notaExame = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                status = "Aluno reprovado.";
+            }
+
+
         }
     }
 }
