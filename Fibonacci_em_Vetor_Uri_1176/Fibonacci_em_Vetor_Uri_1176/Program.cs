fc18ef896cb271fe30cd1cb1f19a246e0096f6be
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-24 14:56:51 -0300

    Fibonacci_em_Vetor_Uri_1176: Finished

--- Fibonacci_em_Vetor_Uri_1176/Fibonacci_em_Vetor_Uri_1176/Program.cs
+++ Fibonacci_em_Vetor_Uri_1176/Fibonacci_em_Vetor_Uri_1176/Program.cs
@@ -6,7 +6,31 @@ namespace Fibonacci_em_Vetor_Uri_1176
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Informe a quantidade de termos da série: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int qtdTermos = 20;
+            int[] vFibo = new int[qtdTermos];
+
+            vFibo[0] = 0;
+            vFibo[1] = 1;
+
+            for (int i = 0; i < qtdTermos; i++)
+            {
+                if (i >= 2)
+                {
+                    vFibo[i] = vFibo[i - 1] + vFibo[i - 2];
+                }
+            }
+
+            //for (int i = 0; i < qtdTermos; i++)
+            //{
+            //    Console.Write(vFibo[i] + " ");
+            //}
+
+            Console.WriteLine("Fib("+n+") = " + vFibo[n]);
+
+
         }
     }
 }
